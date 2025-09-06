cfc88283f3654f361c17546290645090edb16bc3
Matthew Zahra <matthewzahra04@gmail.com>
2025-09-06 12:49:49 +0100

    test

---
+++ agent_test.py
@@ -0,0 +1,17 @@
+from smolagents import CodeAgent, InferenceClientModel, DuckDuckGoSearchTool
+
+import os 
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+# Initialize a model (using Hugging Face Inference API)
+model = InferenceClientModel(token=os.getenv('HUGGINGFACEHUB_API_TOKEN'))  # Uses a default model
+
+# Create an agent with no tools
+agent = CodeAgent(tools=[], model=model)
+
+# Run the agent with a task
+result = agent.run("print the files in the current directory")
+print(result)
